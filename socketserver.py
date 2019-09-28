43757d6fbebfd6115591a4f690a6b6675598e621
Loganyaa <40915723+Loganzi@users.noreply.github.com>
2019-09-28 21:51:49 +0530

    basic server side socket

---
+++ socketserver.py
@@ -0,0 +1,25 @@
+from aiohttp import web
+import socketio
+
+sio = socketio.AsyncServer()
+app = web.Application()
+sio.attach(app)
+
+async def index(request):
+#enter homepage html
+    with open('.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
+@sio.on('message')
+#prints at server terminal
+async def print_message(sid, message):
+    print("Socket ID: " , sid)
+    print(message)
+#codes
+
+
+#attach to app
+app.router.add_get('/', index)
+#start server
+if __name__ == '__main__':
+    web.run_app(app)
